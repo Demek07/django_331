c23ce7e2495a42a015df8a2119ba4cc6145312f8
v <v@mail.ru>
2024-03-05 22:25:24 +0500

    lesson_48: первое представление "привет мир!"

--- cards/views.py
+++ cards/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+# ./cards/views.py
+from django.http import HttpResponse
 
-# Create your views here.
+
+def main(request):
+    return HttpResponse("Привет, мир!")  # вернет страничку с надписью "Привет, мир!" на русском языке.
