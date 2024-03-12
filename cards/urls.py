598c8ebd4492ba3dfa1d8c4fc069965d5c2360f8
v <v@mail.ru>
2024-03-12 21:29:19 +0500

    lesson 50: init

--- cards/urls.py
+++ cards/urls.py
@@ -1,14 +1,13 @@
+# /cards/urls.py
 from django.urls import path
 from . import views
-# cards/urls.py
-# будет иметь префикс в urlах /cards/
 
+# Префикс /cards/
 urlpatterns = [
-    # маршрут детального представления карточки в каталоге cards/catalog/1/
-    path('catalog/<int:card_id>/', views.get_card_by_id, name='get_card_by_id'),
-    # Маршрут категорий cards/catalog/python/
-    path('catalog/<slug:slug>/', views.get_category_by_name, name='get_category_by_name'),
-    path('', views.get_all_cards, name='all_cards'),
-    # маршрут каталога cards/catalog/
-    path('catalog/', views.catalog, name='catalog'),
+    path('catalog/', views.get_all_cards, name='catalog'),  # Общий каталог всех карточек
+    path('/categories/', views.get_categories, name='categories'),  # Список всех категорий
+    path('/categories/<slug:slug>/', views.get_cards_by_category, name='category'),  # Карточки по категории
+    path('/tags/<slug:slug>/', views.get_cards_by_tag, name='tag'),  # Карточки по тегу
+    path('/<int:card_id>/detail/', views.get_detail_card_by_id, name='detail_card_by_id'),
+    # Детальная информация по карточке
 ]
