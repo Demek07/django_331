1202daf2267b887ba14efaea85ce4e1dfa59c37a
v <v@mail.ru>
2024-03-12 20:53:26 +0500

    hw_29: работа с urls.py

--- cards/urls.py
+++ cards/urls.py
@@ -4,6 +4,11 @@ from . import views
 # будет иметь префикс в urlах /cards/
 
 urlpatterns = [
-    path('<int:card_id>/', views.card_by_id, name='card_detail'),
+    # Маршрут категорий cards/catalog/python/
+    path('catalog/<slug:slug>/', views.card_by_id, name='get_category_by_name'),
     path('', views.get_all_cards, name='all_cards'),
+    # маршрут детального представления карточки в каталоге cards/catalog/1/
+    path('catalog/<int:card_id>/', views.card_by_id, name='get_card_by_id'),
+    # маршрут каталога cards/catalog/
+    path('catalog/', views.catalog, name='catalog'),
 ]
