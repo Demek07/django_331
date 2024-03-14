db59127d107197a2447b7926b2ef6b191614be46
v <v@mail.ru>
2024-03-14 23:42:04 +0500

    lesson_51: inclusion_tag (работает)

--- cards/templatetags/markdown_to_html_2.py
+++ cards/templatetags/markdown_to_html_2.py
@@ -8,6 +8,8 @@ register = template.Library()
 # @register это декоратор, который используется в Django для создания простых пользовательских тегов шаблонов.
 # @register.inclusion_tag - это декоратор, который используется для создания пользовательских тегов шаблонов,
 # которые включают шаблон в шаблон.
-@register.inclusion_tag('templates/include/markdown_to_html_tag.html', takes_context=True)
-def markdown_to_html2(markdown_text: str) -> str:
+@register.inclusion_tag('include/markdown_to_html_tag.html', takes_context=True)
+def markdown_to_html2(context, markdown_text: str) -> dict:
+    # Делайте что-то с context, если нужно. В противном случае, его можно игнорировать в теле функции.
+    # Пример возврата значения без использования context напрямую:
     return {'markdown_text': markdown_text.upper() + ' - это тестовый текст'}
