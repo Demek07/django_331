2704ebdd76e2e5875ab301389346a661be4c4879
v <v@mail.ru>
2024-03-21 21:21:59 +0500

    lesson_53: сделал детальное отображение карточки из БД по ID

--- cards/views.py
+++ cards/views.py
@@ -154,17 +154,15 @@ def get_detail_card_by_id(request, card_id):
     """
     Возвращает детальную информацию по карточке для представления
     """
-    # Ищем карточку по id в нашем наборе данных
-    card = None
-    for c in cards_dataset:
-        if c['id_card'] == int(card_id):
-            card = c
-            break
+    # Ищем карточку по id в базе данных
+    card = Card.objects.get(pk=card_id)
 
-    info['card'] = card
-    # Если карточка нашлась - рендерим шаблон с информацией о карточке
-    if card:
-        return render(request, 'cards/card_detail.html', context=info)
-    else:
-        return HttpResponse('Такой карточки нет', status=404)
+    # Подготавливаем контекст и отображаем шаблон
+    context = {
+        'card': card,
+        'menu': info['menu'],
+    }
+
+    return render(request, 'cards/card_detail.html', context)
+
     
