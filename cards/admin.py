3aea270b8583a75d98a2330fc0a4fe6b270221f7
v <v@mail.ru>
2024-04-02 21:30:28 +0500

    lesson_56: базовая кастомизация админки CardAdmin

--- cards/admin.py
+++ cards/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import Card
-
+# admin.site.register(Card) - альтернативный способ регистрации модели
 @admin.register(Card)
 class CardAdmin(admin.ModelAdmin):
-    pass
+    # Поля, которые будут отображаться в админке
+    list_display = ('question', 'views', 'upload_date')
+    # Поля, которые будут ссылками
+    list_display_links = ('question', 'upload_date')
+    # Поля по которым будет поиск
+    search_fields = ('question', 'answer')
+    # Поля по которым будет фильтрация
+    list_filter = ('category',)
+
