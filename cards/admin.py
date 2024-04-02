0f436d6f3aedde84baba4f03b729ce609cb1190b
v <v@mail.ru>
2024-04-02 23:08:50 +0500

    hw_33: actions - пользовательские действия над карточками

--- cards/admin.py
+++ cards/admin.py
@@ -4,7 +4,7 @@ from .models import Card
 @admin.register(Card)
 class CardAdmin(admin.ModelAdmin):
     # Поля, которые будут отображаться в админке
-    list_display = ('id', 'question', 'category', 'views', 'upload_date', 'status')
+    list_display = ('id', 'question', 'category', 'views', 'upload_date', 'status', 'brief_info')
     # Поля, которые будут ссылками
     list_display_links = ('id',)
     # Поля по которым будет поиск
@@ -17,3 +17,22 @@ class CardAdmin(admin.ModelAdmin):
     list_per_page = 25
     # Поля, которые можно редактировать
     list_editable = ('views', 'question', 'status')
+    actions = ['set_checked', 'set_unchecked']
+
+    @admin.action(description="Пометить как проверенное")
+    def set_checked(self, request, queryset):
+        updated_count = queryset.update(status=Card.Status.CHECKED)
+        self.message_user(request, f"{updated_count} записей было помечено как проверенное")
+
+    @admin.action(description="Пометить как не проверенное")
+    def set_unchecked(self, request, queryset):
+        updated_count = queryset.update(status=Card.Status.UNCHECKED)
+        self.message_user(request, f"{updated_count} записей было помечено как непроверенное")
+
+    # Определение метода для отображения краткой информации о карточке
+    @admin.display(description="Наличие кода",
+                   ordering='answer')  # ordering по полю answer, так как точного поля для сортировки по краткому описанию нет
+    def brief_info(self, card):
+        # Проверяем наличие кода
+        has_code = 'Да' if '```' in card.answer else 'Нет'
+        return has_code
