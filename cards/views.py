237f5328a5a5a164689cbd5b0d77a6d0569cc9d8
v <v@mail.ru>
2024-04-09 21:52:20 +0500

    lesson_58: базовая форма для добавления карточек

--- cards/views.py
+++ cards/views.py
@@ -25,65 +25,9 @@ from django.views.decorators.cache import cache_page
 
 from .templatetags.markdown_to_html import markdown_to_html
 
-"""
-Информация в шаблоны будет браться из базы данных
-Но пока, мы сделаем переменные, куда будем записывать информацию, которая пойдет в 
-контекст шаблона
-"""
-
-# Пример данных для карточек
-cards_dataset = [
-    {"question": "Что такое PEP 8?",
-     "answer": "PEP 8 — стандарт написания кода на Python.",
-     "category": "Стандарты кода",
-     "tags": ["PEP 8", "стиль", "форматирование"],
-     "id_author": 1,
-     "id_card": 1,
-     "upload_date": "2023-01-15",
-     "views_count": 100,
-     "favorites_count": 25
-     },
-    {"question": "Как объявить список в Python?",
-     "answer": "С помощью квадратных скобок: lst = []",
-     "category": "Основы",
-     "tags": ["списки", "основы"],
-     "id_author": 2,
-     "id_card": 2,
-     "upload_date": "2023-01-20",
-     "views_count": 150,
-     "favorites_count": 30
-     },
-    {"question": "Что делает метод .append()?",
-     "answer": "Добавляет элемент в конец списка.",
-     "category": "Списки",
-     "tags": ["списки", "методы"],
-     "id_author": 2,
-     "id_card": 3,
-     "upload_date": "2023-02-05",
-     "views_count": 75,
-     "favorites_count": 20
-     },
-    {"question": "Какие типы данных в Python иммутабельные?",
-     "answer": "Строки, числа, кортежи.",
-     "category": "Типы данных",
-     "tags": ["типы данных", "иммутабельность"],
-     "id_author": 1,
-     "id_card": 4,
-     "upload_date": "2023-02-10",
-     "views_count": 90,
-     "favorites_count": 22
-     },
-    {"question": "Как создать виртуальное окружение в Python?",
-     "answer": "С помощью команды: python -m venv myenv",
-     "category": "Виртуальные окружения",
-     "tags": ["venv", "окружение"],
-     "id_author": 2,
-     "id_card": 5,
-     "upload_date": "2023-03-01",
-     "views_count": 120,
-     "favorites_count": 40
-     }
-]
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from .forms import CardForm
 
 info = {
     "users_count": 100500,
@@ -99,8 +43,7 @@ info = {
         {"title": "Каталог",
          "url": "/cards/catalog/",
          "url_name": "catalog"},
-    ],
-    "cards": cards_dataset  # Добавим в контекст шаблона информацию о карточках, чтобы все было в одном месте
+    ], # Добавим в контекст шаблона информацию о карточках, чтобы все было в одном месте
 }
 
 
@@ -149,7 +92,7 @@ def catalog(request):
     # Получаем отсортированные карточки
     # cards = Card.objects.all().order_by(order_by)
 
-    # Получаем карточки из БД в ЖАДНОМ режиме
+    # Получаем карточки из БД в ЖАДНОМ режиме многие ко многим tags
     cards = Card.objects.prefetch_related('tags').order_by(order_by)
 
     # Подготавливаем контекст и отображаем шаблон
@@ -240,3 +183,19 @@ def preview_card_ajax(request):
 
         return JsonResponse({'html': html_content})
     return JsonResponse({'error': 'Invalid request'}, status=400)
+
+
+
+
+def add_card(request):
+    if request.method == 'POST':
+        form = CardForm(request.POST)
+        if form.is_valid():
+            # Здесь вы можете обработать валидные данные формы
+            # Например, сохранить их в базе данных или как-то иначе использовать
+            # Пока что мы просто перенаправим на другую страницу
+            return HttpResponseRedirect('/')
+    else:
+        form = CardForm()
+
+    return render(request, 'cards/add_card.html', {'form': form, 'menu': info['menu']})
