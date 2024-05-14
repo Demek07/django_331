382daa9a029e05160fe167cdb8c539a35c934ef1
v <v@mail.ru>
2024-05-14 23:35:28 +0500

    lesson_64: начал работу над личным кабинетом

--- users/forms.py
+++ users/forms.py
@@ -3,8 +3,9 @@ from re import U
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.core.exceptions import ValidationError
+import datetime
 
 class RegisterUserForm(UserCreationForm):
     password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
@@ -33,3 +34,49 @@ class RegisterUserForm(UserCreationForm):
 class CustomAuthenticationForm(AuthenticationForm):
     username = forms.CharField(label='Имя пользователя | Email', widget=forms.TextInput(attrs={'class': 'form-control'}))
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+
+class ProfileUserForm(forms.ModelForm):
+    this_year = datetime.date.today().year
+    date_birth = forms.DateField(
+        label='Дата рождения',
+        widget=forms.SelectDateWidget(years=range(this_year - 100, this_year - 5)),
+        required=False
+    )
+    photo = forms.ImageField(
+        label='Фотография',
+        required=False
+    )
+    
+    
+    username = forms.CharField(
+        disabled=True,  # Поле не редактируемое
+        label='Логин',
+        widget=forms.TextInput(attrs={'class': 'form-control'})  # Использование Bootstrap класса
+    )
+    email = forms.CharField(
+        disabled=True,  # Поле не редактируемое
+        label='E-mail',
+        widget=forms.TextInput(attrs={'class': 'form-control'})  # Использование Bootstrap класса
+    )
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email', 'first_name', 'last_name', 'date_birth', 'photo']
+        labels = {
+            'first_name': 'Имя',
+            'last_name': 'Фамилия',
+            'date_birth': 'Дата рождения',
+            'photo': 'Фотография'
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'})
+        }
+
+
+class UserPasswordChangeForm(PasswordChangeForm):
+    old_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Старый пароль'}))
+    new_password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Новый пароль'}))
+    new_password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Подтверждение нового пароля'}))
+    
--- users/views.py
+++ users/views.py
@@ -5,10 +5,15 @@ from django.contrib.auth import logout
 from .forms import CustomAuthenticationForm, RegisterUserForm
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
 from django.urls import reverse_lazy
 from cards.views import MenuMixin
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import TemplateView, CreateView, ListView
+from django.contrib.auth import get_user_model
+from django.views.generic.edit import UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import ProfileUserForm, UserPasswordChangeForm
+from cards.models import Card
 
 class LoginUser(MenuMixin, LoginView):
     form_class = CustomAuthenticationForm
@@ -35,3 +40,42 @@ class RegisterUser(CreateView):
 class RegisterDoneView(MenuMixin, TemplateView):
     template_name = 'users/register_done.html'
     extra_context = {'title': 'Регистрация завершена'}
+
+
+class ProfileUser(LoginRequiredMixin, UpdateView):
+    model = get_user_model()  # Используем модель текущего пользователя
+    form_class = ProfileUserForm  # Связываем с формой профиля пользователя
+    template_name = 'users/profile.html'  # Указываем путь к шаблону
+    extra_context = {'title': 'Профиль пользователя','active_tab': 'profile'}  # Дополнительный контекст для передачи в шаблон
+
+    def get_success_url(self):
+        # URL, на который переадресуется пользователь после успешного обновления
+        return reverse_lazy('users:profile')
+
+    def get_object(self, queryset=None):
+        # Возвращает объект модели, который должен быть отредактирован
+        return self.request.user
+
+
+class UserPasswordChange(PasswordChangeView):
+    form_class = UserPasswordChangeForm
+    template_name = 'users/password_change_form.html'
+    extra_context = {'title': 'Изменение пароля',
+                     'active_tab': 'password_change'}
+    success_url = reverse_lazy('users:password_change_done')
+
+
+class UserPasswordChangeDone(TemplateView):
+    template_name = 'users/password_change_done.html'
+    extra_context = {'title': 'Пароль изменен успешно'}
+
+
+class UserCardsView(ListView):
+    model = Card
+    template_name = 'users/profile_cards.html'
+    context_object_name = 'cards'
+    extra_context = {'title': 'Мои карточки',
+                     'active_tab': 'profile_cards'}
+
+    def get_queryset(self):
+        return Card.objects.filter(author=self.request.user).order_by('-upload_date')
