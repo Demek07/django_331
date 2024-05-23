01b3a1851ddd131285c21589fb9df0bce1ab306c
v <v@mail.ru>
2024-05-23 14:55:29 +0500

    lesson_67: обновление моделей для авторизации через соцсети

--- users/models.py
+++ users/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class User(AbstractUser):
     photo = models.ImageField(upload_to='users/images/%Y/%m/%d/', blank=True, null=True, verbose_name='Фотография')
     date_birth = models.DateTimeField(blank=True, null=True, verbose_name='Дата рождения')
+    github_id = models.CharField(max_length=255, blank=True, null=True)
+    vk_id = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return self.username
