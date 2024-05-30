0cb5b22522883aba220b3d4b62d4120df1e9e3e0
v <v@mail.ru>
2024-05-30 23:14:34 +0500

    lesson_69: bug fix class Command

--- users/management/commands/create_moderators_group.py
+++ users/management/commands/create_moderators_group.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
-from your_app_name.models import Card  # Замените 'your_app_name' на имя вашего приложения
+from cards.models import Card 
 
 class Command(BaseCommand):
     help = 'Создание группы Модераторы и назначение права change_card'
